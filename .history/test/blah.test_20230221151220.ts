81d92f7dc8d58c0ab8ee60256f29b1146e94ea22
@七七 <alen9femail@gmail.com>
2023-02-21 15:13:48 +0800

    test(blah.test.ts)

--- .history/test/blah.test_20230221151220.ts
+++ .history/test/blah.test_20230221151220.ts
@@ -1,17 +1,15 @@
 /*
- * @Description:
+ * @Description: 
  * @Autor: 万洲
  * @Date: 2022-11-10 10:30:15
  * @LastEditors: 万洲
  * @LastEditTime: 2022-11-10 10:49:40
  */
-import aaa from '../src/index';
+import aaa from "../src/index"
 
 const testFunctions = () => {
-  console.log('执行结果======');
-};
+  console.log("执行结果======");
+}
 describe('blah', () => {
   it('works', testFunctions);
 });
-
-// haolin test
