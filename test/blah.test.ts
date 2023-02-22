c19429b5bc59d55e34f65de3a4c46dbd1133c541
万洲 <960854884@qq.com>
2023-02-22 11:04:25 +0800

    20230222

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -5,14 +5,9 @@
  * @LastEditors: 万洲
  * @LastEditTime: 2022-11-10 10:49:40
  */
-import aaa from '../src/index';
-
 const testFunctions = () => {
   console.log('执行结果======');
 };
 describe('blah', () => {
   it('works', testFunctions);
 });
-
-// haolin test
-const wanzhou = '真帅!';
